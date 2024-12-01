3c2e1b73a6479344c6f2b2da0c574b65e95cc0e4
Bokmal15 <github@exactum.se>
2024-12-01 18:02:12 +0100

    Det här borde räcka för ljud

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Sound winningSound = new Sound();
+        winningSound.WinningSound();
+        Thread.Sleep(5000);  // Wait for sound to finish. Behövs här eftersom ljuden ligger direkt efter varandra
+        Sound diceThrowSound = new Sound();
+        diceThrowSound.DiceThrowSound();
         Game game = new Game();
         game.StartGame();
     }
---
+++ Sound.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+using static System.Console;
+using System.Media;
+namespace _10000
+{
+    internal class Sound
+    {
+        public void WinningSound()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+
+                using (SoundPlayer player = new SoundPlayer(@"Winning.wav"))
+                {
+                    player.Load();
+                    player.Play(); // PlaySync will block the thread until the sound is complete. Use Play() for asynchronous play.
+                }
+            }
+        }
+        public void DiceThrowSound()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                using (SoundPlayer player = new SoundPlayer(@"DiceThrow.wav"))
+                {
+                    player.Load();
+                    player.Play(); // PlaySync will block the thread until the sound is complete. Use Play() for asynchronous play.
+                }
+            }
+        }
+    }
+}
